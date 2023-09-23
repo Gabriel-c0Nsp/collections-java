6e79891b412b9e45c2d6b1ab0ebd1b7bddd016f6
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2023-09-23 12:11:45 -0300

    Adicionados métodos para adicionar e remover itens da lista

--- List/CarrinhoCompras/src/carrinhocompras/CarrinhoDeCompras.java
+++ List/CarrinhoCompras/src/carrinhocompras/CarrinhoDeCompras.java
@@ -1,22 +1,46 @@
 package carrinhocompras;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarrinhoDeCompras {
+
+  private List<Item> itemList;
+
+  public CarrinhoDeCompras() {
+    this.itemList = new ArrayList<>();
+  }
  
   public void adicionarItem(String nome, double preco, int quantidade) {
-    // TODO: implementar esse método
+    // Adiciona um item ao carrinho com o nome, preço e quantidade especificados.
+    itemList.add(new itemList(nome, preco, quantidade));
   }
 
   public void removerItem(String nome) {
-    // TODO: implementar esse método
+    // Remove um item do carrinho com base no seu nome.
+    List<Item> itensParaRemover = new ArrayList<>();
+
+    if (!itemList.isEmpty()) {
+      for (Item item : itemList) {
+        if (item.getNome().equalsIgnoreCase(nome) && item.getPreco().equalsIgnoreCase(preco) && item.getQuantidade().equalsIgnoreCase(quantidade)) {
+          itensParaRemover.add(item);
+        }
+      }
+      itemList.removeAll(itensParaRemover);
+    } else {
+      System.out.println("A lista está vazia!");
+    }
   }
 
   public double calcularValorTotal() {
     // TODO: implementar esse método
+    // Calcula e retorna o valor total do carrinho, levando em consideração o preço e a quantidade de cada item.
     return 0;
   }
 
   public void exibirItens() {
     // TODO: implementar esse método 
+    // Exibe todos os itens presentes no carrinho, mostrando seus nomes, preços e quantidades.
   }
 
 }
