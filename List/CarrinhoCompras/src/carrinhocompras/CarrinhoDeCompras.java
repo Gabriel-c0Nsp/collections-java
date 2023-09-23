56c8a59acc16ded22eed6265aa33a6e17a9d9738
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2023-09-23 11:57:02 -0300

    Conteúdo de aula e estrutura base da questão criada

---
+++ List/CarrinhoCompras/src/carrinhocompras/CarrinhoDeCompras.java
@@ -0,0 +1,22 @@
+package carrinhocompras;
+
+public class CarrinhoDeCompras {
+ 
+  public void adicionarItem(String nome, double preco, int quantidade) {
+    // TODO: implementar esse método
+  }
+
+  public void removerItem(String nome) {
+    // TODO: implementar esse método
+  }
+
+  public double calcularValorTotal() {
+    // TODO: implementar esse método
+    return 0;
+  }
+
+  public void exibirItens() {
+    // TODO: implementar esse método 
+  }
+
+}
